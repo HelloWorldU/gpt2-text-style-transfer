94b25d393077daa133b909b3e60d5426e50a5359
HelloWorldU <102237385+HelloWorldU@users.noreply.github.com>
2024-09-06 19:59:49 +0800

    Update pre_progress.py

--- pre_progress.py
+++ pre_progress.py
@@ -42,8 +42,8 @@ def set_seed(seed=42) -> tuple:
 # 训练配置
 @dataclass
 class TrainConfig:
-    epochs: int = 2
-    batch_size: int = 2
+    epochs: int = 1
+    batch_size: int = 16
     max_len: int = 0
     accumulation_steps: int = 2
     lambda_rec: float = 1.0
@@ -69,12 +69,12 @@ class TrainConfig:
 
 
 # 读取数据集
-def load_dataset(file_path_X, file_path_Y, test_file_path, tokenizer, split_ratio=0.9, seed=None):
+def load_dataset(file_path_X, file_path_Y, test_file_path, tokenizer, split_ratio=0.8, seed=None):
     if seed is not None:
         random.seed(seed)
 
     # 最大长度截断
-    max_length = 301
+    max_length = 240
 
     def read_and_tokenize(file_path, style):
         with tf.io.gfile.GFile(file_path, 'r') as f:
@@ -200,3 +200,102 @@ def remove_leading_dim(input_ids, attention_mask):
     input_ids = tf.squeeze(input_ids, axis=0)
     attention_mask = tf.squeeze(attention_mask, axis=0)
     return input_ids, attention_mask
+
+
+# 根据设备进行数据分片
+def distribute_data(input_ids, attention_mask, labels, styles):
+    replica_context = tf.distribute.get_replica_context()
+    replica_ids = replica_context.replica_id_in_sync_group
+
+    print(f"replica_device_ids: {replica_ids}")
+
+    """
+    we firstly to reshape the input
+    """
+    actual_shape = tf.shape(input_ids)
+    input_ids = tf.reshape(input_ids, (actual_shape[0] * actual_shape[1], actual_shape[2]))
+    attention_mask = tf.reshape(attention_mask, (actual_shape[0] * actual_shape[1], actual_shape[2]))
+
+    """
+    then, we repeat styles and labels
+    """
+    styles = tf.repeat(styles, repeats=actual_shape[0])
+    labels = tf.repeat(labels, repeats=actual_shape[0], axis=0)
+
+    """
+    Data sharding based on equipment
+    """
+    ids = input_ids[replica_ids * actual_shape[1]: (replica_ids + 1) * actual_shape[1]]
+    mask = attention_mask[replica_ids * actual_shape[1]: (replica_ids + 1) * actual_shape[1]]
+    labels = labels[replica_ids * actual_shape[1]: (replica_ids + 1) * actual_shape[1]]
+    styles = styles[replica_ids * actual_shape[1]: (replica_ids + 1) * actual_shape[1]]
+    return ids, mask, labels, styles
+
+
+def distribute_data_disc_Y(real_ids, real_mask, input_ids, attention_mask):
+    replica_context = tf.distribute.get_replica_context()
+    replica_ids = replica_context.replica_id_in_sync_group
+
+    """
+    reshape and repeat the relevant tensor
+    """
+    actual_real_shape = tf.shape(real_ids)
+    real_ids = tf.reshape(real_ids, (actual_real_shape[0] * actual_real_shape[1], actual_real_shape[2]))
+    real_mask = tf.reshape(real_mask, (actual_real_shape[0] * actual_real_shape[1], actual_real_shape[2]))
+    actual_predict_shape = tf.shape(input_ids)
+    predict_ids = tf.reshape(input_ids, (actual_predict_shape[0] * actual_predict_shape[1], actual_predict_shape[2]))
+    predict_mask = tf.reshape(attention_mask, (actual_predict_shape[0] * actual_predict_shape[1], actual_predict_shape[2]))
+
+    """
+    Data sharding based on equipment
+    """
+    Y_ids = real_ids[replica_ids * actual_real_shape[1]: (replica_ids + 1) * actual_real_shape[1]]
+    Y_mask = real_mask[replica_ids * actual_real_shape[1]: (replica_ids + 1) * actual_real_shape[1]]
+    X_ids = predict_ids[replica_ids * actual_predict_shape[1]: (replica_ids + 1) * actual_predict_shape[1]]
+    X_mask = predict_mask[replica_ids * actual_predict_shape[1]: (replica_ids + 1) * actual_predict_shape[1]]
+    return Y_ids, Y_mask, X_ids, X_mask
+
+
+def distribute_data_disc_Z(input_ids, attention_mask):
+    replica_context = tf.distribute.get_replica_context()
+    replica_ids = replica_context.replica_id_in_sync_group
+
+    """
+    reshape and repeat the relevant tensor
+    """
+    actual_shape = tf.shape(input_ids)
+    input_ids = tf.reshape(input_ids, (actual_shape[0] * actual_shape[1], actual_shape[2]))
+    attention_mask = tf.reshape(attention_mask, (actual_shape[0] * actual_shape[1], actual_shape[2]))
+
+    """
+    Data sharding based on equipment
+    """
+    ids = input_ids[replica_ids * actual_shape[1]: (replica_ids + 1) * actual_shape[1]]
+    mask = attention_mask[replica_ids * actual_shape[1]: (replica_ids + 1) * actual_shape[1]]
+    return ids, mask
+
+
+# 梯度累积步数进行数据分片
+def accumulation_distribute(accumulation_steps, input_ids, attention_mask, labels, styles, step):
+    batch_data = tf.shape(input_ids)[0] // accumulation_steps
+    ids = input_ids[step * batch_data: (step + 1) * batch_data]
+    mask = attention_mask[step * batch_data: (step + 1) * batch_data]
+    labels = labels[step * batch_data: (step + 1) * batch_data]
+    styles = styles[step * batch_data: (step + 1) * batch_data]
+    return ids, mask, labels, styles
+
+
+def accumulation_distribute_disc_Y(accumulation_steps, real_ids, real_mask, input_ids, attention_mask, step):
+    batch_data = tf.shape(real_ids)[0] // accumulation_steps
+    ids = real_ids[step * batch_data: (step + 1) * batch_data]
+    mask = real_mask[step * batch_data: (step + 1) * batch_data]
+    predict_ids = input_ids[step * batch_data: (step + 1) * batch_data]
+    predict_mask = attention_mask[step * batch_data: (step + 1) * batch_data]
+    return ids, mask, predict_ids, predict_mask
+
+
+def accumulation_distribute_disc_Z(accumulation_steps, input_ids, attention_mask, step):
+    batch_data = tf.shape(input_ids)[0] // accumulation_steps
+    ids = input_ids[step * batch_data: (step + 1) * batch_data]
+    mask = attention_mask[step * batch_data: (step + 1) * batch_data]
+    return ids, mask
